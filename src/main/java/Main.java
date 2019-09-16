915e61d5933b4baccdb8059046957baa5d6750f7
xml12333 <xml12333@gmail.com>
2019-09-16 20:33:03 +0300

    homeword part 1 and 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        //1
+        System.out.println("1 string");
+        System.out.println("2 string");
+        System.out.println("3 string");
+        System.out.println("4 string");
+        System.out.println("5 string");
+        //3
         int a = 5;
         int b = 5;
 
-        System.out.println("Area of a rectangle a*b =" +a*b);
+        System.out.println("Area of a rectangle a*b =" + a * b);
     }
 }
