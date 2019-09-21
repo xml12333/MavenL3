b5aa9f8f5471d2f1b8defe4ac7d6ea4847d04ae7
xml12333 <xml12333@gmail.com>
2019-09-21 22:23:15 +0300

    homeword part 1-9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,112 +3,138 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         //task 1
-        System.out.println("TASK 1");
-        System.out.println("1 string");
-        System.out.println("2 string");
-        System.out.println("3 string");
-        System.out.println("4 string");
-        System.out.println("5 string");
-        //task2
-        System.out.println("TASK 2");
-        boolean a2 = true;
-        byte b2 = 100;
-        char c2 = 'a';
-        short d2 = 255;
-        int e2 = 10000;
-        long f2 = 66000;
-        float g2 = 5.14f;
-        double h2 = 6.9999999995;
+        //"1. Print numbers from 10 to 20 using all known cycles.
+        int a = 10;
+        int b = 20;
+        while (a <= b) {
+            System.out.println(a++);
+        }
+        a = 10;
+        do {
+            System.out.println(a++);
+        } while (a <= b);
 
+        for (a = 10; a <= b; a++) {
+            System.out.println(a);
+        }
+        int[] c = new int[11];
+        int d = 10;
+        for (int i : c) {
+            System.out.println(d++);
+        }
+        //task 2
+        //"2. Print all the numbers from 1 to 100, which are divided by 3 without a remainder.
+        int e;
+        for (e = 1; e <= 100; e++) {
+            if (e % 3 == 0) {
+                System.out.println(e);
+            }
+        }
         //task 3
-        System.out.println("TASK 3");
-        int a3 = 5;
-        int b3 = 5;
+        //"3. Fill the array with numbers from 100 to 0.
+        int[] f = new int[101];
+        int g = 0;
+        for (int k = 100; k >= 0; k--)
+        {
+            f[g] = k;
+            //System.out.println(f[g]);
+            g++;
 
-        System.out.println("Area of a rectangle a*b =" + a3 * b3);
-
+        }
         //task 4
-        //4. Fix the program to get the correct result:
-        System.out.println("TASK 4");
-        int a4 = 5;
-        int b4 = 10;
-        float result4 = a4 / b4;
-        System.out.println(result4);
+        //4. Given an array of 10 integers. To display the sum of all its elements except the first and last.
+        int[] l = new int[10];
+        for (int i =0;i<10;i++){
+            l[i]=i;
+//            System.out.println(l[i]);
+        }
+        int n =0;
+        for (int m=0;m<10;m++){
+            if (m==0||m==9)
+            {
+                continue;
+            }
+            n=n+l[m];
+        }
+        System.out.println(n);
 
-        // task 5
-        //"5. Change the operations  to get the result 20:
-        System.out.println("TASK 5");
-        int a5 = 1;
-        int b5 = 3;
-        int c5 = 9;
-        int d5 = 27;
-
-        int result5 = (-a5 + (b5 - c5)) + (d5);
-
-        System.out.println(result5);
+        //task 5
+        //"5. Write a function which calculates and returns the minimum of 4 arguments.
+        Main obj = new Main();
+        System.out.println(obj.min(100,9,10,11));
 
         //task 6
-        //"6. Comment redundant strings to get the output ""Happy Java Learning"":
-        System.out.println("TASK 6");
-        //String s6 = ""Java"";
-        //System.out.println(""Happy"");
-        //System.out.println(""Java Learning"");
-        //System.out.println(""programming"");
-        System.out.print("Happy Java");
-        //System.out.println(""weekend"");
-        //System.out.println(s);
-        //System.out.print(""Python"");
-        System.out.print(" ");
-        System.out.println("Learning");
+        //6. Write a method named getEvenDigitSum with one parameter of the type "int".
+        // The method should return the sum of even digits within the number.
+        // If the number is negative, method should return -1;
+        System.out.println(getEvenDigitSum(9));
 
         //task 7
-        //"7. Comment variables which are never used.
-        System.out.println("TASK 7");
-        int a = 10;
-        int b = 15;
-        double c = b + 38;
-        //int d = a + 12;
-        //double e = 12.3;
-        String s = "s" + a;
-        String s1 = a + "b";
-        //String s2 = ""a"";
-        String s3 = s1 + "a";
-        String s4 = s3 + "b";
-        System.out.println(c + s4 + s);
-
-        //task 8
-        //"8*. Write a program which prints ""Hello world"" to the console using methods transformIntToChar and printList only.
-        // write your code here
-        System.out.println("TASK 8");
-        Main obj = new Main();
-        obj.printList(obj.transformIntToChar(72));
-        obj.printList(obj.transformIntToChar(101 ));
-        obj.printList(obj.transformIntToChar(108 ));
-        obj.printList(obj.transformIntToChar(108 ));
-        obj.printList(obj.transformIntToChar(111 ));
-        obj.printList(obj.transformIntToChar(32 ));
-        obj.printList(obj.transformIntToChar(119 ));
-        obj.printList(obj.transformIntToChar(111 ));
-        obj.printList(obj.transformIntToChar(114 ));
-        obj.printList(obj.transformIntToChar(108 ));
-        obj.printList(obj.transformIntToChar(100 ));
-
-
-
+        // "7. Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
+        //If one of the parameters is < 10, method should return -1 to indicate an invalid value.
+        //The method should return the greatest common divisor of two numbers."
+        System.out.println(getGreatestCommonDivisor(18,30));
+    }
+    public static int min(int a, int b, int c, int d) {
+        if (min(a,b)==b){a = b;}
+        if (min(a,c)==c){a = c;}
+        if (min(a,d)==d){a = d;}
+        return a;
+    }
+    public static int min(int a, int b) {
+        if (a < b){return a;}
+        else {return b;}
     }
 
-    private static ArrayList<Character> transformIntToChar(int... values) {
-        ArrayList<Character> buffer = new ArrayList<>();
-        for (int i: values) {
-            buffer.add(((char) i));
+    public static int getEvenDigitSum(int a){
+        if (a<0 ) {
+            return -1;
         }
-        return buffer;
+
+        int[] evenArray = fillEvenDigit(a);
+        int result = 0;
+        for (int i : evenArray) {
+            result = result + evenArray[i];
+        }
+        return result;
+    }
+    public static int[] fillEvenDigit(int a){
+        int [] array = new int[a+1];
+        for (int e = 1; e <= a; e++) {
+            if (e % 2 == 0) {
+                array[e] = e;
+            }
+        }
+        return array;
     }
 
-    private static void printList(ArrayList<Character> characters) {
-        for (char c: characters) {
-            System.out.print(c);
+    public static int getGreatestCommonDivisor(int a, int b){
+        if (a<10||b<10) {
+            return -1;
         }
+        int result = 0;
+        int mm = min(a,b);
+        int[] m1 = returnDivisorArray(a);
+        int[] m2 = returnDivisorArray(b);
+        for(int i=0;i<=mm;i++){
+            if(m1[i]!=0 && m2[i]!=0 && m1[i]==m2[i]){
+                result = i;
+            }
+//            System.out.print(m1[i]);
+//            System.out.print(",");
+//            System.out.println(m2[i]);
+        }
+        return result;
+    }
+    public static int[] returnDivisorArray(int a){
+        int[] m = new int[a+1];
+        for(int i = 1;i<=a;i++){
+            if(a%i==0){
+            m[i]=i;}
+        }
+        return m;
     }
 
 }
+
+
