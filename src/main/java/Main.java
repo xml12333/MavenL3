ffaaac2c2534baa610d4c57694c619a793a6cada
xml12333 <xml12333@gmail.com>
2019-10-03 21:38:28 +0300

    homework 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,111 +1,100 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         //task 1
-        double v1,v2,t1,t2,k1,k2;
-        v1=100;
-        v2=50;
-        t1 = 36.6;
-        t2 = 38;
-        k1 = v1/v2;
-        k2 = t1/t2;
-        System.out.println("V="+(v1+v2)/2*k1);
-        System.out.println("T="+(t1+t2)/2*k2);
-        //task 2
-        double evro,dollar,funt;
-        evro = 35.38802342;
-        dollar = 23.234231344234;
-        funt = 45.23412345;
-        Scanner obj = new Scanner (System.in);
-        System.out.println("Enter summ:");
-        double summ = obj.nextDouble();
-        System.out.println("In evro summ:"+ summ*evro);
-        System.out.println("In dollar summ:"+ summ*dollar);
-        System.out.println("In funt summ:"+ summ*funt);
-        //task 3
-        int a,b,a4,a6;
-        a = 125;
-        b = a*a;
-        a4 = b*b;
-        a6= b*b*b;
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        for (int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i));
+
         //task 4
-        int a1=100;
-        int b2=200;
-        int c;
-
-        c = a1;
-        a1 = b2;
-        b2 = c;
-
-        a1 = a1+b2;
-        b2 = a1-b2;
-        a1 = a1-b2;
-
-
-
-    }
-    public static int min(int a, int b, int c, int d) {
-        if (min(a,b)==b){a = b;}
-        if (min(a,c)==c){a = c;}
-        if (min(a,d)==d){a = d;}
-        return a;
-    }
-    public static int min(int a, int b) {
-        if (a < b){return a;}
-        else {return b;}
-    }
-
-    public static int getEvenDigitSum(int a){
-        if (a<0 ) {
-            return -1;
-        }
-
-        int[] evenArray = fillEvenDigit(a);
-        int result = 0;
-        for (int i : evenArray) {
-            result = result + evenArray[i];
-        }
-        return result;
-    }
-    public static int[] fillEvenDigit(int a){
-        int [] array = new int[a+1];
-        for (int e = 1; e <= a; e++) {
-            if (e % 2 == 0) {
-                array[e] = e;
+        //Create an array of 15 random integers from the segment [0;   9].
+        // Display the array on the screen. Count how many in the array of even elements and
+        // display this number on the screen on a separate line.
+        int A[] = new int[15];
+        int B[] = new int[15];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = randomWithRange(0, 9);
+            System.out.print(A[i]);
+            if (A[i] % 2 == 0) {
+                B[i] = A[i];
             }
         }
-        return array;
-    }
-
-    public static int getGreatestCommonDivisor(int a, int b){
-        if (a<10||b<10) {
-            return -1;
-        }
-        int result = 0;
-        int mm = min(a,b);
-        int[] m1 = returnDivisorArray(a);
-        int[] m2 = returnDivisorArray(b);
-        for(int i=0;i<=mm;i++){
-            if(m1[i]!=0 && m2[i]!=0 && m1[i]==m2[i]){
-                result = i;
+        System.out.println();
+        for(int i=0; i<B.length;i++) {
+            if (B[i] != 0) {
+                System.out.println(B[i]);
             }
-//            System.out.print(m1[i]);
-//            System.out.print(",");
-//            System.out.println(m2[i]);
         }
-        return result;
-    }
-    public static int[] returnDivisorArray(int a){
-        int[] m = new int[a+1];
-        for(int i = 1;i<=a;i++){
-            if(a%i==0){
-                m[i]=i;}
+        //task 5
+        //"5. Create ArrayList of Strings. Add 5 different Strings to it.
+        //Using any cycle print all the values of the ArrayList to the console."
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("One");
+        list2.add("Two");
+        list2.add("Three");
+        list2.add("Four");
+        System.out.println();
+        System.out.println(list2.size());
+        for (int i = 0; i < list2.size(); i++)
+            System.out.println(list2.get(i));
+
+        //task 6
+        //"6. Create ArrayList of Strings. Add 10 different Strings to it.
+        //Using any cycle find the longest String in the list and print it out.
+        //If there are several String with the same legth - print each from a new line."
+        ArrayList<String> list3 = new ArrayList<String>();
+        for(int i = 0; i<=10; i++)
+            list3.add("String "+i);
+        int max = 0;
+        int x;
+        for(int i = 1; i<11;i++){
+            x = list3.get(i).length();
+            if (max < x) {
+                max = x;
+            }
         }
-        return m;
+        for (int i = 1; i<11;i++) {
+            x = list3.get(i).length();
+            if (max == x) {
+                System.out.println(list3.get(i));
+            }
+        }
+        //task 9
+        //"9. Create ArrayList<Integer> and add 100 random numbers (Use Math.random()).
+        //Create HashSet<Integer> and add all unique numbers from the ArrayList."
+        ArrayList<Integer> int1 = new ArrayList<Integer>();
+        Set intHashSet = new HashSet();
+        System.out.println();
+        for(int i=0;i<100;i++) {
+            int1.add(randomWithRange(0,1000));
+            System.out.print(int1.get(i) +" ");
+            intHashSet.add(int1.get(i));
+        }
+        retrieveValuesFromListMethod1(intHashSet);
+
     }
 
+    public static void retrieveValuesFromListMethod1(Set set)
+    {
+        Iterator itr = set.iterator();
+        while(itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+    }
+
+    private static int randomWithRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
 }
 
 
